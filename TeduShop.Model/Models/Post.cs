255fe90cf7750060b1571239ee0d7db99bb421f3
WINDOWS10\Nam Nguyen <nhocksok333@gmail.com>
2018-09-28 20:56:31 +0700

    update

--- TeduShop.Model/Models/Post.cs
+++ TeduShop.Model/Models/Post.cs
@@ -1,6 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using TeduShop.Model.Abstract;
+
 
 namespace TeduShop.Model.Models
 {
@@ -37,5 +40,6 @@ namespace TeduShop.Model.Models
 
         [ForeignKey("CategoryID")]
         public virtual PostCategory PostCategory { set; get; }
+        public virtual IEnumerable<PostTag> PostTags { get; set; }
     }
 }
