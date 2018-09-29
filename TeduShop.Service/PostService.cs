21ed4e4eb9606612c5b884bbe6b742c6c2d916c0
WINDOWS10\Nam Nguyen <nhocksok333@gmail.com>
2018-09-29 14:34:11 +0700

    update unitTest

--- TeduShop.Service/PostService.cs
+++ TeduShop.Service/PostService.cs
@@ -12,8 +12,9 @@ namespace TeduShop.Service
         void Delete(int id);
         IEnumerable<Post> GetAll();
         IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow);
+        IEnumerable<Post> GetAllByCategoryPaging(int categoryId, int page, int pageSize, out int totalRow);
         Post GetById(int id);
-        IEnumerable<Post> GetAllByTagPaging(int page, int pagesize, out int totalRow);
+        IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pagesize, out int totalRow);
         void SaveChanges();
     }
     class PostService : IPostService
@@ -40,10 +41,15 @@ namespace TeduShop.Service
             return _postRepository.GetAll(new string[] {"PostCategory"});
         }
 
-        public IEnumerable<Post> GetAllByTagPaging(int page, int pageSize, out int totalRow)
+        public IEnumerable<Post> GetAllByCategoryPaging(int categoryId, int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => x.Status && x.CategoryID == categoryId, out totalRow, page, pageSize,new string[] { "PostCategory" });
+        }
+
+        public IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow)
         {
             //TODO: Select all post by tag
-            return _postRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageSize);
+            return _postRepository.GetAllByTag(tag, page, pageSize, out totalRow);
         }
 
         public IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow)
